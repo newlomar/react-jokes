3459c6906d5d9a654efdfad958d60cff5d87bc47
Newton Lomar <newtonlomar16@gmail.com>
2022-08-10 13:00:09 -0300

    app setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Joke setup='' punchline='' />
+      <Joke setup='' punchline='' />
+      <Joke setup='' punchline='' />
+      <Joke setup='' punchline='' />
+      <Joke setup='' punchline='' />
+    </div>
+  );
 }
 
 export default App;

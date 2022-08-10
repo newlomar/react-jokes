81b34f22a89afb4d858d82fd077587b02e5496ae
Newton Lomar <newtonlomar16@gmail.com>
2022-08-10 14:09:14 -0300

    initial development of joke component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import Joke from './Joke/Joke';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <Joke setup='' punchline='' />
-      <Joke setup='' punchline='' />
-      <Joke setup='' punchline='' />
-      <Joke setup='' punchline='' />
-      <Joke setup='' punchline='' />
+      <Joke setup='setup1' punchline='punchline1' />
+      <Joke setup='setup2' punchline='punchline2' />
+      <Joke setup='setup3' punchline='punchline3' />
+      <Joke setup='setup4' punchline='punchline4' />
+      <Joke setup='setup5' punchline='punchline5' />
     </div>
   );
 }
---
+++ src/Joke/Joke.jsx
@@ -0,0 +1,9 @@
+export default function Joke({ setup, punchline }) {
+  return (
+    <section>
+      <p>Hi, I'm a joke!</p>
+      <p>This is a setup: {setup}</p>
+      <p>This is a setup: {punchline}</p>
+    </section>
+  );
+}

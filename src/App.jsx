ff7a3a7399be13d190ab263ebb2b73f38b91a62e
Newton Lomar <newtonlomar16@gmail.com>
2022-08-14 10:11:49 -0300

    adding array and map to show the jokes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import Joke from './Joke/Joke';
 import './App.css';
+import jokes from './jokes.json';
 
 function App() {
+  const jokesArray = jokes.jokes;
   return (
     <div className='app'>
-      <Joke
-        setup='I got my son a fridge for his birthday'
-        punchline={"I can't wait to see her face light up when she opens it"}
-      />
-      <Joke
-        setup='How did the hacker escape the police?'
-        punchline='He just ransomware!'
-      />
-      <Joke
-        setup={"Why don't pirates travel on mountain roads?"}
-        punchline='Scurvy.'
-      />
-      <Joke
-        setup='Why do bees stay in the hive in the winter?'
-        punchline='Swarm.'
-      />
-      <Joke
-        setup={"What's the best thing about Switzerland?"}
-        punchline={"I don't know, but the flag is a big plus!"}
-      />
+      {jokesArray.map((joke) => (
+        <Joke key={joke.setup} setup={joke.setup} punchline={joke.punchline} />
+      ))}
     </div>
   );
 }

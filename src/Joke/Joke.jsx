835ce17bac7bc4098b38415489d5c1c9ede5888d
Newton Lomar <newtonlomar16@gmail.com>
2022-11-15 21:37:52 -0300

    adding condition rendering challenge text, its solution and refactoring conditional redering setup

--- src/Joke/Joke.jsx
+++ src/Joke/Joke.jsx
@@ -1,11 +1,26 @@
+import React from "react";
+
 export default function Joke({ setup, punchline }) {
+  /**
+    * Challenge:
+    * - Create state `isShown` (boolean, default to `false`)
+    * - Add a button that toggles the value back and forth
+  */
+  const [isShown, setIsShown] = React.useState(false)
+  console.log(isShown);
+
+  const toggle = () => {
+    setIsShown(prevState => !prevState);
+  }
+
   return (
     <section className=''>
       <div className='joke__card'>
-        {setup ? <p>{setup}</p> : ''}
+        {setup && <p>{setup}</p>}
         <p>
           <b>{punchline}</b>
         </p>
+        <button onClick={toggle}>Show / Hide</button>
       </div>
     </section>
   );
